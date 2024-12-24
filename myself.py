d3421b74fc70ea74c60d513a0d928dca4860ad01
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-12-24 22:01:09 +0600

    Create myself.py

---
+++ myself.py
@@ -0,0 +1,24 @@
+# Bismillahir Rahmanir Raheem
+
+# * * * * Coder   : Abu Bakar Istiak
+# * * * * Age     : 21
+# * * * * Location: Chattogram, Bangladesh
+# * * * * Institute: Chattogram Polytechnic Institute
+# * * * * Ambition: To become a Software Development Engineer
+# * * * * Dream   : Joining a big tech company like Google, Microsoft, or Meta
+# * * * * Created : 2024-12-24 || 21:09:14
+# * * * * File    : My_Details.py
+
+def my_details():
+    print("Bismillahir Rahmanir Raheem")
+    print("Coder   : Abu Bakar Istiak")
+    print("Age     : 21")
+    print("Location: Chattogram, Bangladesh")
+    print("Institute: Chattogram Polytechnic Institute")
+    print("Ambition: To become a Software Development Engineer")
+    print("Dream   : Joining a big tech company like Google, Microsoft, or Meta")
+
+if __name__ == "__main__":
+    my_details()
+
+# Alhamdulillah
